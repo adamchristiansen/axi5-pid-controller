626b6297f33d04ad6bc55acd0d789aaebf975dab
Adam Christiansen <hello@adamc.io>
2025-04-30 11:42:50 -0600

    refactor: remove pid_ prefix from test signals

--- pid_controller_tb.tcl
+++ pid_controller_tb.tcl
@@ -10,34 +10,34 @@ set upper [expr $width - 1]
 set lower 0
 
 # Signal names.
-set pid_aclk            "top.pid_controller_tb.pid_aclk"
-set pid_aresetn         "top.pid_controller_tb.pid_aresetn"
-set pid_s_axis_e_tdata  "top.pid_controller_tb.pid_s_axis_e_tdata\[$upper:$lower\]"
-set pid_s_axis_e_tvalid "top.pid_controller_tb.pid_s_axis_e_tvalid"
-set pid_m_axis_u_tdata  "top.pid_controller_tb.pid_m_axis_u_tdata\[$upper:$lower\]"
-set pid_m_axis_u_tvalid "top.pid_controller_tb.pid_m_axis_u_tvalid"
-set pid_kp              "top.pid_controller_tb.pid_kp\[$upper:$lower\]"
-set pid_ki              "top.pid_controller_tb.pid_ki\[$upper:$lower\]"
-set pid_kd              "top.pid_controller_tb.pid_kd\[$upper:$lower\]"
+set aclk            "top.pid_controller_tb.aclk"
+set aresetn         "top.pid_controller_tb.aresetn"
+set s_axis_e_tdata  "top.pid_controller_tb.s_axis_e_tdata\[$upper:$lower\]"
+set s_axis_e_tvalid "top.pid_controller_tb.s_axis_e_tvalid"
+set m_axis_u_tdata  "top.pid_controller_tb.m_axis_u_tdata\[$upper:$lower\]"
+set m_axis_u_tvalid "top.pid_controller_tb.m_axis_u_tvalid"
+set kp              "top.pid_controller_tb.kp\[$upper:$lower\]"
+set ki              "top.pid_controller_tb.ki\[$upper:$lower\]"
+set kd              "top.pid_controller_tb.kd\[$upper:$lower\]"
 
 # Add signals.
 gtkwave::/Edit/Insert_Comment "Clock & Reset"
-gtkwave::addSignalsFromList [list $pid_aclk $pid_aresetn]
+gtkwave::addSignalsFromList [list $aclk $aresetn]
 gtkwave::/Edit/Insert_Blank
 gtkwave::/Edit/Insert_Comment "Error Signal"
-gtkwave::addSignalsFromList [list $pid_s_axis_e_tdata $pid_s_axis_e_tvalid]
+gtkwave::addSignalsFromList [list $s_axis_e_tdata $s_axis_e_tvalid]
 gtkwave::/Edit/Insert_Blank
 gtkwave::/Edit/Insert_Comment "Control Variable"
-gtkwave::addSignalsFromList [list $pid_m_axis_u_tdata $pid_m_axis_u_tvalid]
+gtkwave::addSignalsFromList [list $m_axis_u_tdata $m_axis_u_tvalid]
 gtkwave::/Edit/Insert_Blank
 gtkwave::/Edit/Insert_Comment "PID Coefficients"
-gtkwave::addSignalsFromList [list $pid_kp $pid_ki $pid_kd]
+gtkwave::addSignalsFromList [list $kp $ki $kd]
 
 # Set all of the listed waveforms to fixed-point.
 #
 # Loop over each waveform instead of highlighting all and manipulating them as a batch. Some
 # commands do not work as a batch and instead only modify the most recently highlighted waveform.
-foreach name [list $pid_s_axis_e_tdata $pid_m_axis_u_tdata $pid_kp $pid_ki $pid_kd] {
+foreach name [list $s_axis_e_tdata $m_axis_u_tdata $kp $ki $kd] {
     gtkwave::highlightSignalsFromList [list $name]
     gtkwave::/Edit/Data_Format/Signed_Decimal
     gtkwave::/Edit/Data_Format/Fixed_Point_Shift/Specify $radix
@@ -50,7 +50,7 @@ foreach name [list $pid_s_axis_e_tdata $pid_m_axis_u_tdata $pid_kp $pid_ki $pid_
 #
 # Loop over each waveform instead of highlighting all and manipulating them as a batch. Some
 # commands do not work as a batch and instead only modify the most recently highlighted waveform.
-foreach name [list $pid_s_axis_e_tdata $pid_m_axis_u_tdata] {
+foreach name [list $s_axis_e_tdata $m_axis_u_tdata] {
     gtkwave::highlightSignalsFromList [list $name]
     gtkwave::/Edit/Data_Format/Analog/Step
     gtkwave::/Edit/Insert_Analog_Height_Extension
